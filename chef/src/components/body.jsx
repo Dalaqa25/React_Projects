19ab302a7e94725b751d77ab6426b3aa14e39bad
Dalaqa25 <g.dalaqishvili01@gmail.com>
2025-01-20 23:33:29 +0400

    dads

--- chef/src/components/body.jsx
+++ chef/src/components/body.jsx
@@ -1,14 +1,34 @@
+import React from 'react';
+
 function Body() {
+
+    const [ingredients, setIngredients] = React.useState([])
+
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function addIngredient(formData) {
+        const newIngredient = formData.get("ingredient")
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+    }
+
   return (
-    <main>
-        <form className="add-ingrediance-from" action="">
-            <input 
-                type="text" 
-                placeholder="e.g. oregano" 
-            />
-            <button>Add ingredient</button>
-        </form>
-    </main>
+    <>
+        <main>
+            <form action={ addIngredient } className="add-ingrediance-from">
+                <input 
+                    type="text" 
+                    name="ingredient"
+                    placeholder="e.g. oregano" 
+                />
+                <button>Add ingredient</button>
+            </form>
+        </main>
+        <ul>
+            { ingredientsListItems }
+        </ul>
+    </>
   );
 }
 
