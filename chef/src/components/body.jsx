4e59c8a8f06f193ee3f7d836aa95a861638b3c5b
Dalaqa25 <g.dalaqishvili01@gmail.com>
2025-01-21 19:33:48 +0400

    das

--- chef/src/components/body.jsx
+++ chef/src/components/body.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
 function Body() {
-
+    //states 
     const [ingredients, setIngredients] = React.useState([])
+    //state for recipe
+    const [visibleRecipe, setVisibleRecipe] = React.useState(false)
 
     const ingredientsListItems = ingredients.map(ingredient => (
         <li key={ingredient}>{ingredient}</li>
     ))
+
+    //visible function
+    function showRecipe() {
+        if (ingredientsListItems.length >= 0) {
+            setVisibleRecipe(true)
+        }
+    }
 
     function addIngredient(formData) {
         const newIngredient = formData.get("ingredient")
@@ -21,15 +30,28 @@ function Body() {
                     type="text" 
                     name="ingredient"
                     placeholder="e.g. oregano" 
+                    required
                 />
-                <button>Add ingredient</button>
+                <button onClick={showRecipe}>Add ingredient</button>
             </form>
         </main>
-        <ul>
-            <li>
-                { ingredientsListItems }
-            </li>
-        </ul>
+        <div style={{ display: visibleRecipe ? 'block' : 'none' }}>
+            <div className='section-container' >
+                <section>
+                    <h2>Ingreients on hand:</h2>
+                    <ul>
+                        { ingredientsListItems }
+                    </ul>
+                    <div className='get-recipe-container'>
+                        <div>
+                            <h4>Ready for recipe?</h4>
+                            <p>Generate a recipe from your list of ingredients.</p>
+                        </div>
+                        <button>Get recipe</button>
+                    </div>
+                </section>
+            </div>
+        </div>
     </>
   );
 }
